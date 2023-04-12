809ee375653665f08de0e1e04a1ee84f9692bcd9
jwstats <j.will.3993@gmail.com>
2023-04-12 09:20:27 -0400

    Connecting to a Database

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.sessions import engine
+from db.base_class import Base
 
-app = FastAPI(title=settings.PROJECT_TITLE, varion=settings.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, varion=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
 
 @app.get("/")
 def hello_api():
