57e03485389f3b1c1814175344495ae56c113e93
jwstats <j.will.3993@gmail.com>
2023-05-09 00:21:06 -0400

    Create a homepage view of all jobs, Create a detailed view of specific job

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from db.repository.jobs import list_jobs
+from sqlalchemy.orm import Session
+from db.session import get_db
+from fastapi import Depends
+from db.repository.jobs import retreive_job
 
 
 templates = Jinja2Templates(directory="templates")
@@ -9,6 +14,11 @@ router = APIRouter()
 
 
 @router.get("/")
-def home(request: Request):
-    dir(request)
-    return templates.TemplateResponse("jobs/homepage.html", {"request": request})
+def home(request: Request, db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html", {"request": request, "jobs": jobs})
+
+@router.get("/detail/{id}")
+def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    return templates.TemplateResponse("jobs/detail.html", {"request": request, "job": job})
