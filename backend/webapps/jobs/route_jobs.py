66320617e441973cf0e29cda149d0665d0c873e8
jwstats <j.will.3993@gmail.com>
2023-05-11 23:59:46 -0400

    Implement Job Create and Delete functionality Frontend

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -5,8 +5,16 @@ from fastapi.templating import Jinja2Templates
 from db.repository.jobs import list_jobs
 from sqlalchemy.orm import Session
 from db.session import get_db
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status, responses
 from db.repository.jobs import retreive_job
+
+from webapps.jobs.forms import JobCreateForm
+from fastapi.security.utils import get_authorization_scheme_param
+from apis.version1.route_login import get_current_user_from_token
+from schemas.jobs import JobCreate
+from db.models.users import User
+from db.repository.jobs import create_new_job  
+
 
 
 templates = Jinja2Templates(directory="templates")
@@ -22,3 +30,37 @@ def home(request: Request, db: Session = Depends(get_db), msg: str = None):
 def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
     job = retreive_job(id=id, db=db)
     return templates.TemplateResponse("jobs/detail.html", {"request": request, "job": job})
+
+@router.get("/post-a-job/")  
+def create_job(request: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("jobs/create_job.html", {"request": request})
+ 
+ 
+@router.post("/post-a-job/")
+async def create_job(request: Request, db: Session = Depends(get_db)):
+    form = JobCreateForm(request)
+    await form.load_data()
+    if form.is_valid():
+        try:
+            token = request.cookies.get("access_token")
+            scheme, param = get_authorization_scheme_param(
+                token
+            )  # scheme will hold "Bearer" and param will hold actual token value
+            current_user: User = get_current_user_from_token(token=param, db=db)
+            job = JobCreate(**form.__dict__)
+            job = create_new_job(job=job, db=db, owner_id=current_user.id)
+            return responses.RedirectResponse(
+                f"/detail/{job.id}", status_code=status.HTTP_302_FOUND
+            )
+        except Exception as e:
+            print(e)
+            form.__dict__.get("errors").append(
+                "You might not be logged in, In case problem persists please contact us."
+            )
+            return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
+    return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
+
+@router.get("/delete-job")
+def show_jobs_to_delete(request: Request, db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/show_jobs_to_delete.html", {"request": request, "jobs": jobs})
